ed6f58083c5171884dac892e52ce1e3f663af5d9
harune-pg <h.makino.22.6.533@4nonome.com>
2024-08-22 18:15:05 +0900

    Applyed lint

--- records/urls.py
+++ records/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "records"
 
 urlpatterns = [
-    path('status/', views.LabStatusView.as_view(), name='lab_status'),
-    path('enter/', views.EnterLabView.as_view(), name='enter_lab'),
-    path('exit/', views.ExitLabView.as_view(), name='exit_lab'),
+    path("status/", views.LabStatusView.as_view(), name="lab_status"),
+    path("enter/", views.EnterLabView.as_view(), name="enter_lab"),
+    path("exit/", views.ExitLabView.as_view(), name="exit_lab"),
 ]
